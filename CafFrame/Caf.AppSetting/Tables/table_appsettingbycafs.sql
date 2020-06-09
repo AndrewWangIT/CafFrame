530f7e86cc91919210b6e83c56c63d1a855ae34e
JohnnyQianP <qianpengabc@126.com>
2020-06-09 12:30:12 +0800

    appsetting

--- CafFrame/Caf.AppSetting/Tables/table_appsettingbycafs.sql
+++ CafFrame/Caf.AppSetting/Tables/table_appsettingbycafs.sql
@@ -1,7 +1,9 @@
 ﻿
-DROP TABLE IF EXISTS AppSettingByCafs
-CREATE TABLE AppSettingByCafs (
-    [Id] [bigint] IDENTITY(1,1) NOT NULL,
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[AppSettingByCafs]') AND OBJECTPROPERTY(id, N'ISUSERTABLE') = 1)
+DROP TABLE [dbo].[AppSettingByCafs]
+
+CREATE TABLE [dbo].[AppSettingByCafs](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[CreateTime] [datetime2](3) NOT NULL,
 	[CreateUserId] [nvarchar](100) NULL,
 	[IsDeleted] [bit] NOT NULL,
@@ -9,5 +11,13 @@ CREATE TABLE AppSettingByCafs (
 	[Value] [nvarchar](max) NULL,
 	[Description] [nvarchar](1000) NULL,
 	[LatestModifiedTime] [datetime2](3) NULL,
-    CONSTRAINT [PK_AppSettingByCafs] PRIMARY KEY CLUSTERED ( [Id] ASC )
-);
+ CONSTRAINT [PK_AppSettingByCafs] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [AppSettingByCafs_Key] UNIQUE NONCLUSTERED 
+(
+	[Key] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
